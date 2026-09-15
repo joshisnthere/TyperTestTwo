e2c0b0442079e2aa99f65dfff4d96c696830ec0b
Joshua Mutavi <alolagamerza@gmail.com>
2026-09-14 23:26:10 -0500

    Polish and fixes

---
+++ typing_stats.py
@@ -0,0 +1,9 @@
+"""
+Scoring and local history for the typing test. History is stored as JSON
+next to this file so it survives between runs without any external service.
+"""
+
+import datetime
+import json
+import os
+import random
